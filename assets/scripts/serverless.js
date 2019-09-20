fcd3b0f994b0d48a5c0d22ebc2e5cd36abf192d7
RajatRasal <yugiohrajat1@gmail.com>
2019-09-20 15:04:22 +0100

    1) Add desc and date to homepage image 2) Style for desc and date on homepage

--- assets/scripts/serverless.js
+++ assets/scripts/serverless.js
@@ -1,12 +1,37 @@
 function getAndSetImageOfTheDay() {
-  const url = 'https://us-central1-my-blog-253414.cloudfunctions.net/get_image_from_bucket';
+  const url = 'https://europe-west2-my-blog-253414.cloudfunctions.net/get_image_from_bucket';
 
-  var setPic = function(image) { 
-    var picOfTheDay = document.getElementsByClassName('picOfTheDayHolder')[0];
+  var setPic = function(image, desc, date) { 
+    console.log('Image:' + image);
+    console.log('Desc:' + desc);
+    console.log('Date:' + date);
+    var picOfTheDayHolder = document.getElementsByClassName('picture-otd-holder')[0];
+
+    /* New Image */
     newImg = document.createElement('img');
-    newImg.className = 'picOfTheDay';
+    newImg.className = 'picture-otd';
     newImg.src = image;
-    picOfTheDay.appendChild(newImg)
+
+    /* New Image Description */
+    postInfo = document.createElement('span');
+    postInfo.className = 'image-information';  // index-page';
+    /* Image Description */
+    postInfoHeading = document.createElement('h4');
+    postInfoHeading.innerHTML = desc;
+    /* Image Date */
+    postInfoDate = document.createElement('span');
+    postInfoDate.className = 'image-date';
+    postInfoDate.innerHTML = date;
+
+    /* Inserting all new elements in the right places */
+    picOfTheDayHolder.appendChild(newImg)
+    picOfTheDayHolder.appendChild(postInfo)
+    postInfo.appendChild(postInfoHeading);
+    postInfo.appendChild(postInfoDate);
+
+	  /*
+	  <span class="post-information"><h3>Sunrise in the Forest! Sort of…..</h3> <span class="date"> July 8, 2019 </span> </span>
+	  */
   };
 
   fetch(url, {mode: 'cors'})
@@ -15,11 +40,13 @@ function getAndSetImageOfTheDay() {
     })
     .then(function(image) {
       console.log('Request successful', text);
-      setPic(image);
+      setPic(image, 'tmp-placeholder', 'date-placeholder');
     })
     .catch(function(error) {
       console.log('Request failed', error) 
       const image = "https://storage.cloud.google.com/blog-image-gallery/staring_cat.jpg"
-      setPic(image);
+      const desc = "Cat Staring out of the Window"
+      const date = "July 2018"
+      setPic(image, desc, date);
     });
 }
--- index.md
+++ index.md
@@ -2,13 +2,12 @@
 layout: default
 title: Rajat
 ---
-<h5>😎<span class="not-error">Welcome to my blog!</span>🧐 As you can see this whole thing is <span class="error">still under construction</span> but feel free to browse around anyways.</h5>
+<h5 class="index-page">😎<span class="not-error">Welcome to my blog!</span>🧐 As you can see this whole thing is <span class="error">still under construction</span> but feel free to browse around anyways.</h5>
 
 <br>
 
-<h5 align="center"><u>Pic of the Day:</u></h5>
-
-<div class="picOfTheDayHolder" align="center"></div>
+<h5 align="center index-page" align="center"><u>A pic from my gallery:</u></h5>
+<div class="picture-otd-holder" align="center"></div>
 
 <script type="text/javascript">
    getAndSetImageOfTheDay(); 
